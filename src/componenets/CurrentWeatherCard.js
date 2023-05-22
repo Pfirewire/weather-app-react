ef362b76676850b3c72d5a981845d6abd59882e3
Steve Pflug <stephen@pflug.us>
2023-05-22 08:35:54 -0500

    added component to show current weather

---
+++ src/componenets/CurrentWeatherCard.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function CurrentWeatherCard() {
+    return(
+        <CurrentWeatherCardWrapper>
+            Current Weather Card
+        </CurrentWeatherCardWrapper>
+    );
+}
+
+export default CurrentWeatherCard;
+
+const CurrentWeatherCardWrapper = styled.div`
+  
+`;
